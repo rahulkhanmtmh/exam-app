443680b4bf24aa745a6a6bcc8132122ecf8eeb6a
rahulkhanmtmh <41508118+rahulkhanmtmh@users.noreply.github.com>
2026-03-23 19:18:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,31 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
 
+app.use(cors({
+  origin: "*"
+}));
+
+app.use(express.json());
+
+// Temporary storage
+let questions = [];
+
+// Test route
 app.get("/", (req, res) => {
   res.send("Backend running");
 });
 
-app.post("/upload", (req, res) => {
-  res.json({ text: "OCR coming soon" });
+// Add question
+app.post("/add-question", (req, res) => {
+  const q = req.body;
+  questions.push(q);
+  res.json({ message: "Question added" });
+});
+
+// Get all questions
+app.get("/questions", (req, res) => {
+  res.json(questions);
 });
 
 app.listen(5000, () => console.log("Server running"));
